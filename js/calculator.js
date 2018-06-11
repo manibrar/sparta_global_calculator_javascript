7b8f39d56d2b1cc843f1dd1ca6e5c87d795349f2
manibrar <manvirbrar1988@gmail.com>
2018-06-11 20:41:17 +0100

    addition prompt

---
+++ js/calculator.js
@@ -0,0 +1,18 @@
+  console.log("inside calculator");
+
+  var x = prompt('Please insert first number for calculation');
+  var y = prompt('Please insert second number for calculation');
+  var z = parseInt(x)+parseInt(y)
+  var addition = alert(z);
+
+
+
+
+
+
+    // get the user inputs
+    // var value1 = prompt('Please insert first number for calculation:');
+    // var value2 = prompt('Please insert first number for calculation:');
+    // var choice = prompt('Choose your action (a)dd (s)ubtract (m)ultiply (d)ivide') || "a";
+    //
+    // alert("your answer is ");
