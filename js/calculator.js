5d370ff747b117d805a65b84108be8d4dab729dc
manibrar <manvirbrar1988@gmail.com>
2018-06-12 00:35:20 +0100

    prompt calc with quit and restart

--- js/calculator.js
+++ js/calculator.js
@@ -2,18 +2,18 @@
 do {
 
   var value1 = parseFloat(prompt('Please insert first number for calculation:'));
+  var choice = (prompt('Choose your action Add(+), Minus(-) Multiply(*) Divide(/)'));
   var value2 = parseFloat(prompt('Please insert second number for calculation:'));
-  var choice = (prompt('Choose your action (a)dd (s)ubtract (m)ultiply (d)ivide'));
-  if(choice == 'a'){
-    alert(value1 + value2);
-  }else if(choice == 's'){
-    alert(value1 - value2);
-  }else if(choice == 'm'){
-    alert(value1*value2);
-  }else if(choice == 'd'){
-    alert(value1/value2);
+  if(choice == '+'){
+    alert("your answer was " + (value1 + value2));
+  }else if(choice == '-'){
+    alert("your answer was " + (value1 - value2));
+  }else if(choice == '*'){
+    alert("your answer was " + (value1*value2));
+  }else if(choice == '/'){
+    alert("your answer was " + (value1/value2));
   }else{
-    alert(value1+value2)
+    alert("your answer was " + (value1+value2));
   }
   var user = prompt('New calculation? (y) or Quit? (n)')
   if (user == 'y') {
