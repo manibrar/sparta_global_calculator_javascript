4697717f10d9180145163f94a85a9dd8f5f1200e
manibrar <manvirbrar1988@gmail.com>
2018-06-12 01:41:51 +0100

    session counters

--- js/calculator.js
+++ js/calculator.js
@@ -11,28 +11,31 @@
     var value1 = parseFloat(prompt('Please insert first number for calculation:'));
     var choice = (prompt('Choose your action Add(+), Minus(-) Multiply(*) Divide(/)'));
     var value2 = parseFloat(prompt('Please insert second number for calculation:'));
+    console.log(value1, choice, value2);
     if (choice == '+') {
       alert("your answer was " + (value1 + value2));
-      console.log("your answer was " + (value1 + value2));
+      console.log("The answer is " + (value1 + value2));
     } else if (choice == '-') {
       alert("your answer was " + (value1 - value2));
-      console.log("your answer was " + (value1 - value2));
+      console.log("The answer is " + (value1 - value2));
     } else if (choice == '*') {
       alert("your answer was " + (value1 * value2));
-      console.log("your answer was " + (value1 - value2));
+      console.log("The answer is " + (value1 - value2));
     } else if (choice == '/') {
       alert("your answer was " + (value1 / value2));
-      console.log("your answer was " + (value1 / value2));
+      console.log("The answer is " + (value1 / value2));
     } else {
       alert("your answer was " + (value1 + value2));
-      console.log("your answer was " + (value1 + value2));
+      console.log("The answer is " + (value1 + value2));
     }
+    console.log("end of session " + x)
     var user = prompt('New calculation? (y) or Quit? (n)')
     if (user == 'n') {
       alert('Thanks for using ManiCalc');
     } else {
-      console.log("end of session " + x)
+
     }
     var x = y;
+
 
   } while (user == 'y');
